122d4d57e05c4dd0b905901f8218994eb1c75752
Surya <phxntomcodm@gmail.com>
2024-07-31 12:23:31 +0530

    Pre cleanup

--- public/bingo.js
+++ public/bingo.js
@@ -2,145 +2,188 @@ const arr = [[], [], [], [], []];
 const strikeoff = [[], [], [], [], []];
 const gameMap = new Map();
 
-const login_screen = document.getElementById('loginpage');
-const game_screen = document.getElementById('table');
-const bingo_table = document.getElementById('bingotable');
-const chat = document.getElementById('chatpage');
+const login_screen = document.getElementById("loginpage");
+const game_screen = document.getElementById("table");
+const bingo_table = document.getElementById("bingotable");
+const chat = document.getElementById("chatpage");
 
-chat.style.display = 'none';
-game_screen.style.display = 'none';
-bingo_table.style.display = 'none';
+const nickNameForm = document.getElementById("nickNameForm");
+const joinRoomForm = document.getElementById("joinRoomForm");
+const createRoomForm = document.getElementById("createRoomForm");
 
-window.clickFunc=function(i, j) {
-    const element = document.getElementById(`${i}x${j}`);
-    let content = element.innerHTML;
-    content = `<span style='color:red'>${content}</span>`;
-    element.innerHTML = content;
-    strikeoff[i][j] = true;
-    console.log(strikeoff);
-    checkBingo();
-}
+nickNameForm.style.display = "block";
+joinRoomForm.style.display = "none";
+createRoomForm.style.display = "none";
 
-export function startBingo(event, gameCode) {
-    event.preventDefault(); 
+chat.style.display = "none";
+game_screen.style.display = "none";
+bingo_table.style.display = "none";
 
-    login_screen.style.display = 'none';
-    game_screen.style.display = '';
-    bingo_table.style.display = '';
-    chat.style.display = '';
+window.clickFunc = function (i, j) {
+  const element = document.getElementById(`${i}x${j}`);
+  let content = element.innerHTML;
+  content = `<span style='color:red'>${content}</span>`;
+  element.innerHTML = content;
+  strikeoff[i][j] = true;
+  console.log(strikeoff);
+  checkBingo();
+};
 
-    const inputfield = document.getElementById("NickName");
-    let nick = inputfield.value.trim();
-    if (nick === "") {
-        document.getElementById("incaseError").innerHTML = "Please enter a name";
-        return;
+export function startBingo(event) {
+  event.preventDefault();
+
+  login_screen.style.display = "none";
+  game_screen.style.display = "";
+  bingo_table.style.display = "";
+  chat.style.display = "";
+
+  const inputfield = document.getElementById("NickName");
+  let nick = inputfield.value.trim();
+  if (nick === "") {
+    document.getElementById("incaseError").innerHTML = "Please enter a name";
+    return;
+  }
+  window.nick = nick;
+  console.log(nick);
+
+  document.getElementById("printrandomnum").innerHTML = getRandomArbitrary(
+    1,
+    25
+  );
+
+  const gameCode = window.hash;
+  console.log(`hashed gamecode is : ${gameCode}`);
+  document.getElementById(
+    "printnickname"
+  ).innerHTML = `Welcome to the game: ${nick}<br>Code: ${gameCode}`;
+  document.getElementById("loginpage").remove();
+  document.getElementById("welcomeheading").innerHTML = "Welcome To Bingo!";
+
+  const uni = Array.from({ length: 25 }, (_, i) => i + 1); // 1 to 25
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      const index = getRandomArbitrary(0, uni.length);
+      arr[i][j] = uni[index];
+      uni.splice(index, 1);
     }
-    window.nick = nick;
-    console.log(nick);
+  }
+  console.log(arr);
 
-    document.getElementById("printrandomnum").innerHTML=getRandomArbitrary(1,25)
-
-    document.getElementById("printnickname").innerHTML = `Welcome to the game: ${nick}<br>Code: ${gameCode}`;
-    document.getElementById("loginpage").remove();
-    document.getElementById("welcomeheading").innerHTML = "Welcome To Bingo!";
-
-    const uni = Array.from({ length: 25 }, (_, i) => i + 1); // 1 to 25
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
-            const index = getRandomArbitrary(0, uni.length);
-            arr[i][j] = uni[index];
-            uni.splice(index, 1);
-        }
+  let html = "<table><tbody>";
+  for (let i = 0; i < 5; i++) {
+    html += "<tr>";
+    for (let j = 0; j < 5; j++) {
+      html += `<td style="border: 1px solid black">`;
+      let str = String(arr[i][j]);
+      html += `<p class='bingotablebuttons' id="${i}x${j}" onclick="clickFunc(${i},${j})">${str}</p>`;
+      html += "</td>";
     }
-    console.log(arr);
-    
-    let html = "<table><tbody>";
-    for (let i = 0; i < 5; i++) {
-        html += '<tr>';
-        for (let j = 0; j < 5; j++) {
-            html += `<td style="border: 1px solid black">`;
-            let str = String(arr[i][j]);
-            html += `<p class='bingotablebuttons' id="${i}x${j}" onclick="clickFunc(${i},${j})">${str}</p>`;
-            html += "</td>";
-        }
-        html += "</tr>";
-    }
-    html += "</table></tbody>";
-    document.getElementById("printtable").innerHTML = html;
+    html += "</tr>";
+  }
+  html += "</table></tbody>";
+  document.getElementById("printtable").innerHTML = html;
 }
 
 export function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getRandomCode() {
-    return Math.floor(1000 + Math.random() * 9000);
+  return Math.floor(1000 + Math.random() * 9000);
 }
 
-window.checkBingo = function() {
-    const bingoString = "BINGO";
-    let totalcount = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        let rowcount = 0;
-        let columncount = 0;
+window.checkBingo = function () {
+  const bingoString = "BINGO";
+  let totalcount = 0;
 
-        for (let j = 0; j < 5; j++) {
-            if (strikeoff[i][j]) rowcount++;
-            if (strikeoff[j][i]) columncount++;
-        }
+  for (let i = 0; i < 5; i++) {
+    let rowcount = 0;
+    let columncount = 0;
 
-        if (rowcount === 5) totalcount++;
-        if (columncount === 5) totalcount++;
+    for (let j = 0; j < 5; j++) {
+      if (strikeoff[i][j]) rowcount++;
+      if (strikeoff[j][i]) columncount++;
     }
 
-    if (strikeoff[0][0] && strikeoff[1][1] && strikeoff[2][2] && strikeoff[3][3] && strikeoff[4][4]) {
-        totalcount++;
-    }
-    if (strikeoff[0][4] && strikeoff[1][3] && strikeoff[2][2] && strikeoff[3][1] && strikeoff[4][0]) {
-        totalcount++;
-    }
+    if (rowcount === 5) totalcount++;
+    if (columncount === 5) totalcount++;
+  }
 
-    if (totalcount > 5) {
-        totalcount = 5;
-    }
+  if (
+    strikeoff[0][0] &&
+    strikeoff[1][1] &&
+    strikeoff[2][2] &&
+    strikeoff[3][3] &&
+    strikeoff[4][4]
+  ) {
+    totalcount++;
+  }
+  if (
+    strikeoff[0][4] &&
+    strikeoff[1][3] &&
+    strikeoff[2][2] &&
+    strikeoff[3][1] &&
+    strikeoff[4][0]
+  ) {
+    totalcount++;
+  }
 
-    console.log("The total count is: ", totalcount);
-    for (let i = 1; i < totalcount+1; i++) {
-        document.getElementById(i.toString()).innerHTML = bingoString[i-1];
-    }
-}
+  if (totalcount > 5) {
+    totalcount = 5;
+  }
+
+  console.log("The total count is: ", totalcount);
+  for (let i = 1; i < totalcount + 1; i++) {
+    document.getElementById(i.toString()).innerHTML = bingoString[i - 1];
+  }
+};
 
 export function CreateRoomFunction(e) {
-    let gameCode;
-    do {
-        gameCode = getRandomCode();
-    } while (gameMap.has(gameCode));
+  e.preventDefault();
 
-    gameMap.set(gameCode, 1);
-    console.log(gameCode);
-    console.log("Number of users in room", gameMap.get(gameCode));
-    startBingo(e, gameCode);
+  let gameCode;
+  do {
+    gameCode = getRandomCode();
+  } while (gameMap.has(gameCode));
+
+  gameMap.set(gameCode, 1);
+  console.log(gameCode);
+  console.log("Number of users in room", gameMap.get(gameCode));
+  //adding the game code to the URL
+  window.location.href += `#${gameCode}`;
+  startBingo(e);
 }
 
 export function joinRoomFunction(e) {
-    const roomCodeInputField = document.getElementById('codeInput');
-    const roomCode = roomCodeInputField.value;
+  e.preventDefault();
+  enterCodeInput.style.display = "";
+  inputTag.style.display = "";
+  const roomCodeInputField = document.getElementById("codeInput");
+  const roomCode = roomCodeInputField.value;
 
-    if (!gameMap.has(roomCode)) {
-        console.log("Room does not exist");
-        document.getElementById('incaseRommFull').innerHTML = "Room does not exist, re-enter a valid code.";
-        return;
-    }
+  if (!gameMap.has(roomCode)) {
+    console.log("Room does not exist");
+    document.getElementById("incaseRommFull").innerHTML =
+      "Room does not exist, re-enter a valid code.";
+    return;
+  }
 
-    let userCount = gameMap.get(roomCode);
-    if (userCount >= 2) {
-        console.log("Room is full");
-        document.getElementById('incaseRommFull').innerHTML = "Room is full, re-enter a new code.";
-        return;
-    }
+  let userCount = gameMap.get(roomCode);
+  if (userCount >= 2) {
+    console.log("Room is full");
+    document.getElementById("incaseRommFull").innerHTML =
+      "Room is full, re-enter a new code.";
+    return;
+  }
 
-    gameMap.set(roomCode, userCount + 1);
-    console.log("Number of users in room", gameMap.get(roomCode));
-    startBingo(e, roomCode);
+  gameMap.set(roomCode, userCount + 1);
+  console.log("Number of users in room", gameMap.get(roomCode));
+  window.location.href += `#${gameCode}`;
+  startBingo(e);
+}
+
+export function onUserLogin() {
+  nickNameForm.style.display = "none";
+  joinRoomForm.style.display = "block";
+  createRoomForm.style.display = "block";
 }
--- public/socket.js
+++ public/socket.js
@@ -1,48 +1,66 @@
-import {SOCKET_MESSAGE_TYPES} from "./GLOBAL_CONSTANTS.js";
-import {CreateRoomFunction, getRandomArbitrary, joinRoomFunction, startBingo} from "./bingo.js";
+import { SOCKET_MESSAGE_TYPES } from "./GLOBAL_CONSTANTS.js";
+import {
+  CreateRoomFunction,
+  getRandomArbitrary,
+  onUserLogin,
+  joinRoomFunction,
+  startBingo,
+} from "./bingo.js";
 const socket = io();
 
-const messageform = document.getElementById('messageform');
-const loginform = document.getElementById('loginform')
-const input = document.getElementById('input');
-const messages = document.getElementById('messages');
-const createRoomButton=document.getElementById('createRoom');
-const joinRoomButton=document.getElementById('joinRoom');
-const nextNumberButton=document.getElementById('nextnumber');
+const messageform = document.getElementById("messageform");
+const input = document.getElementById("input");
 
-const {CHAT_MESSAGE} = SOCKET_MESSAGE_TYPES;
+const messages = document.getElementById("messages");
+const nextNumberButton = document.getElementById("nextnumber");
 
-createRoomButton.addEventListener('click',(e)=>{
-    CreateRoomFunction(e)
-})
+const { CHAT_MESSAGE } = SOCKET_MESSAGE_TYPES;
 
-joinRoomButton.addEventListener('click',(e)=>{
-    joinRoomFunction(e)
-})
+const joinRoomForm = document.getElementById("joinRoomForm");
+const nickNameForm = document.getElementById("nickNameForm");
+const createRoomForm = document.getElementById("createRoomForm");
 
+nickNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  onUserLogin();
+  return false;
+});
 
-loginform.addEventListener('submit',(e)=>{
-    joinRoomFunction(e)
-})
+createRoomForm.addEventListener("submit", (e) => {
+  CreateRoomFunction(e);
+});
 
-nextNumberButton.addEventListener('click',(e)=>{
-    document.getElementById('printrandomnum').innerHTML=getRandomArbitrary(1,25);
-})
+// joinRoomButton.addEventListener('click',(e)=>{
+//     joinRoomFunction(e)
+// })
 
-messageform.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit(CHAT_MESSAGE.label, CHAT_MESSAGE.toStr(window.nick, input.value));
-        input.value = '';
-    }
+joinRoomForm.addEventListener("submit", (e) => {
+  joinRoomFunction(e);
+});
+
+nextNumberButton.addEventListener("click", (e) => {
+  document.getElementById("printrandomnum").innerHTML = getRandomArbitrary(
+    1,
+    25
+  );
+});
+
+messageform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit(
+      CHAT_MESSAGE.label,
+      CHAT_MESSAGE.toStr(window.nick, input.value)
+    );
+    input.value = "";
+  }
 });
 
 socket.on(CHAT_MESSAGE.label, (msg) => {
-    const item = document.createElement('li');
-    const messageData = CHAT_MESSAGE.fromStr(msg);
+  const item = document.createElement("li");
+  const messageData = CHAT_MESSAGE.fromStr(msg);
 
-
-    item.textContent = `${messageData.username}: ${messageData.message}`;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+  item.textContent = `${messageData.username}: ${messageData.message}`;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
